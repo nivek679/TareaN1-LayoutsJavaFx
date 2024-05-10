30273668adc091e75fc830059cf73d7aa2027308
nivek679 <kevinherrera976@gmail.com>
2024-05-10 10:18:27 -0500

    "Se implemento el ratio a la imagen, se ordeno los contenedores. Faltan los perfiles."

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -13,6 +14,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -32,7 +34,24 @@ public class App extends Application {
         ImageView imageView = new ImageView();
         Image image = new Image("/resources/img/ImagenProfile.png");
         imageView.setImage(image);
+        imageView.setFitWidth(50); // Ancho máximo deseado
+        imageView.setPreserveRatio(true); // Mantener la relación de aspecto de la imagen
 
+        StackPane imageViewContainer = new StackPane(imageView);
+        imageViewContainer.setMaxWidth(10);    
+        imageViewContainer.setMaxHeight(40);
+
+        imageViewContainer.setPadding(new Insets(10));
+        contenedorSChats.setPadding(new Insets(10));
+        contenedorSnodos.setPadding(new Insets(10));
+        contenedorSnodos.setSpacing(10);
+        contenedorPerfil.setPadding(new Insets(10));
+        //mensajes_recibidos.setPadding(new Insets(10));
+        //mensajeIngresado.setPadding(new Insets(10));
+        nombrePerfilUsuario.setPadding(new Insets(10)); 
+
+
+        
         root.setTopAnchor(contenedorPrincipal, 0.0);
         root.setBottomAnchor(contenedorPrincipal, 0.0);
         root.setLeftAnchor(contenedorPrincipal, 0.0);
@@ -41,16 +60,10 @@ public class App extends Application {
         
         contenedorPrincipal.getChildren().addAll(contenedorSChats, contenedorSnodos);
         contenedorSChats.getChildren().add(contenedorPerfil);
-        contenedorPerfil.getChildren().addAll(imageView, nombrePerfilUsuario);
+        contenedorPerfil.getChildren().addAll(imageViewContainer, nombrePerfilUsuario);
         contenedorSnodos.getChildren().addAll(mensajes_recibidos, mensajeIngresado);
         root.getChildren().add(contenedorPrincipal);
 
-        
-        
-        
-        
-        
-        
 
         Scene scene = new Scene(root);
         pantallaPrincipal.setTitle("Whatsapp");
